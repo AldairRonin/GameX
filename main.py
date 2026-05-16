0b7dc57def00032ba3b9cab2698428d988e6ece8
AldairRonin <skied25@gmail.com>
2026-05-17 00:29:09 +0500

    Написал базовый обработчик команды /start

---
+++ main.py
@@ -0,0 +1,16 @@
+import asyncio
+from aiogram import Bot, Dispatcher
+from config import BOT_TOKEN
+from handlers.start import router
+
+bot = Bot(token=BOT_TOKEN)
+dp = Dispatcher()
+
+dp.include_router(router)
+
+async def main():
+    print("GameX bot started")
+    await dp.start_polling(bot)
+
+if __name__ == "__main__":
+    asyncio.run(main())
