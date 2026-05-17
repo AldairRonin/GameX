9370c499716355915c0259d38fe2620ade2d3b3c
AldairRonin <skied25@gmail.com>
2026-05-17 23:51:47 +0500

    Написал базовый обработчик команды /start

--- handlers/start.py
+++ handlers/start.py
@@ -1,11 +1,14 @@
 from aiogram import Router
 from aiogram.filters import CommandStart
 from aiogram.types import Message
+from keyboards.main_menu import main_keyboard
+
 
 router = Router()
 
 @router.message(CommandStart())
 async def start_command(message: Message):
     await message.answer(
-        "GameX Запущен!\n\n"
+        "GameX Запущен!\n\n",
+        reply_markup = main_keyboard
     )
