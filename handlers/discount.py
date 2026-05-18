5882552dcdff96ca37027357cb20073255d4864f
AldairRonin <skied25@gmail.com>
2026-05-18 11:23:37 +0500

    Добавил API функцию для нахождение выгодных скидок в Стим

--- handlers/discount.py
+++ handlers/discount.py
@@ -1,10 +1,26 @@
 from aiogram import Router, F
 from aiogram.types import Message
+from services.cheapshark_api import get_top_discounts
 
 router = Router()
 
 @router.message(F.text == "🔥 Топ скидки")
 async def start_command(message: Message):
-    await message.answer(
-        "Тут должны будуть появиться 10 игр со скидками,отсортированные по количеству отзывов в стиме"
-    )
+
+    deals = await get_top_discounts()
+    response_text = "🔥 Топ скидок:\n\n"
+
+    for game in deals:
+        title = game["title"]
+        sale_price = game["salePrice"]
+        normal_price = game["normalPrice"]
+        savings = round(float(game["savings"]))
+
+        response_text += (
+            f"🎮 {title}\n"
+            f"💸 ${sale_price} (вместо ${normal_price})\n"
+            f"🔥 Скидка: {savings}%\n\n"
+        )
+
+
+    await message.answer(response_text)
---
+++ services/cheapshark_api.py
@@ -0,0 +1,16 @@
+import aiohttp
+
+BASE_URL = "https://www.cheapshark.com/api/1.0/deals"
+
+async def get_top_discounts():
+    params = {
+        "storeID": 1,
+
+        "pageSize": 10,
+        "sortBy": "Deal Rating"
+    }
+
+    async with aiohttp.ClientSession() as session:
+        async with session.get(BASE_URL, params=params) as response:
+            return await response.json()
+
