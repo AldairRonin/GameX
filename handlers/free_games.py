2cbf6899cfed46a61e141f793a91749e2afae98e
AldairRonin <skied25@gmail.com>
2026-05-18 11:37:43 +0500

    Добавил API функцию для нахождение игр со 100% скидкой в Стим

--- handlers/free_games.py
+++ handlers/free_games.py
@@ -1,10 +1,25 @@
 from aiogram import Router, F
 from aiogram.types import Message
+from services.cheapshark_api import get_free_games
 
 router = Router()
 
 @router.message(F.text == "🆓 Бесплатные игры")
 async def free_games(message: Message):
-    await message.answer(
-        "Тут должны появиться недавние бесплатные игры"
-    )
+    games = await get_free_games()
+    if not games:
+        await message.answer("🆓 Сейчас бесплатных игр не найдено.")
+        return
+
+    response_text = "🆓 Бесплатные игры:\n\n"
+
+    for game in games:
+        title = game["title"]
+        normal_price = game["normalPrice"]
+
+        response_text += (
+            f"🎮 {title}\n"
+            f"💸 Было: ${normal_price}\n"
+            f"🆓 Сейчас бесплатно!\n\n"
+        )
+    await message.answer(response_text)
--- services/cheapshark_api.py
+++ services/cheapshark_api.py
@@ -14,3 +14,14 @@ async def get_top_discounts():
         async with session.get(BASE_URL, params=params) as response:
             return await response.json()
 
+async def get_free_games():
+    params = {
+        "storeID": 1,
+        "upperPrice":0,
+        "pageSize": 10,
+
+    }
+
+    async with aiohttp.ClientSession() as session:
+        async with session.get(BASE_URL,params=params) as response:
+            return  await response.json()
