24dba042a58a2158f1a3244a68b17ceb71d2dbc2
Zhalgas Sarsenov <zhalgassarsenov9@gmail.com>
2026-05-19 17:30:19 +0500

    Teat

--- handlers/upcoming.py
+++ handlers/upcoming.py
@@ -1,24 +1,77 @@
 from aiogram import Router, F
-from aiogram.types import Message
+from aiogram.types import Message, CallbackQuery
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+
 from services.rawg_api import get_upcoming_games
 
 router = Router()
 
+
+def create_keyboard(page: int):
+    buttons = []
+
+    if page > 1:
+        buttons.append(
+            InlineKeyboardButton(
+                text="⬅️",
+                callback_data=f"upcoming_{page - 1}"
+            )
+        )
+
+    buttons.append(
+        InlineKeyboardButton(
+            text="➡️",
+            callback_data=f"upcoming_{page + 1}"
+        )
+    )
+
+    return InlineKeyboardMarkup(inline_keyboard=[buttons])
+
+
 @router.message(F.text == "🚀 Предстоящие игры")
 async def upcoming_games(message: Message):
-    games = await get_upcoming_games()
+
+    page = 1
+    games = await get_upcoming_games(page)
 
     response_text = "🚀 Предстоящие игры:\n\n"
 
     for game in games:
         title = game["name"]
-        release_date = game["released"]
-        rating = game["rating"]
+        released = game.get("released", "Неизвестно")
 
         response_text += (
             f"🎮 {title}\n"
-            f"📅 Выход: {release_date}\n\n"
+            f"📅 {released}\n\n"
         )
 
-    await message.answer(response_text)
+    await message.answer(
+        response_text,
+        reply_markup=create_keyboard(page)
+    )
 
+
+@router.callback_query(F.data.startswith("upcoming_"))
+async def change_page(callback: CallbackQuery):
+
+    page = int(callback.data.split("_")[1])
+
+    games = await get_upcoming_games(page)
+
+    response_text = "🚀 Предстоящие игры:\n\n"
+
+    for game in games:
+        title = game["name"]
+        released = game.get("released", "Неизвестно")
+
+        response_text += (
+            f"🎮 {title}\n"
+            f"📅 {released}\n\n"
+        )
+
+    await callback.message.edit_text(
+        response_text,
+        reply_markup=create_keyboard(page)
+    )
+
+    await callback.answer()
