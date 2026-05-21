7cc0c12f1f6bfa5498d949a7498e5a289718c5d1
AldairRonin <skied25@gmail.com>
2026-05-22 00:44:20 +0500

    Добавил доп функцию для кнопки:Топ скидки способ сортировки

--- handlers/discount.py
+++ handlers/discount.py
@@ -7,70 +7,105 @@ from services.cheapshark_api import get_top_discounts
 router = Router()
 
 
-def get_keyboard(page: int):
+sort_keyboard = InlineKeyboardMarkup(
+    inline_keyboard=[
+        [
+            InlineKeyboardButton(
+                text="⭐ Выгода",
+                callback_data="sort_Deal Rating"
+            )
+        ],
+        [
+            InlineKeyboardButton(
+                text="🆕 Новые",
+                callback_data="sort_Recent"
+            )
+        ]
+    ]
+)
+
+
+def get_keyboard(page: int, sort_by: str):
     buttons = []
 
     if page > 0:
         buttons.append(
             InlineKeyboardButton(
                 text="⬅️",
-                callback_data=f"discount_{page - 1}"
+                callback_data=f"discount_{page - 1}_{sort_by}"
             )
         )
 
     buttons.append(
         InlineKeyboardButton(
             text="➡️",
-            callback_data=f"discount_{page + 1}"
+            callback_data=f"discount_{page + 1}_{sort_by}"
         )
     )
 
     return InlineKeyboardMarkup(inline_keyboard=[buttons])
 
 
-async def format_discounts(page: int):
-    deals = await get_top_discounts(page)
+async def format_discounts(page: int, sort_by: str = "Deal Rating"):
+    deals = await get_top_discounts(page, sort_by)
 
     response_text = "🔥 Топ скидок:\n\n"
 
     for game in deals:
         title = game["title"]
-        sale_price = float(game["salePrice"])
-        normal_price = float(game["normalPrice"])
+        sale_price = game["salePrice"]
+        normal_price = game["normalPrice"]
         savings = round(float(game["savings"]))
-        kz_sale = round(sale_price * 471)
-        kz_normal = round(normal_price * 471)
 
         response_text += (
             f"🎮 {title}\n"
-            f"💸 ₸{kz_sale} (вместо ₸{kz_normal})\n"
+            f"💸 ${sale_price} (вместо ${normal_price})\n"
             f"🔥 Скидка: {savings}%\n\n"
         )
 
     return response_text
 
 
+
 @router.message(F.text == "🔥 Топ скидки")
 async def discount(message: Message):
+    await message.answer(
+        "📊 Выберите способ сортировки:",
+        reply_markup=sort_keyboard
+    )
+
+
+
+
+@router.callback_query(F.data.startswith("sort_"))
+async def choose_sort(callback: CallbackQuery):
+    sort_by = callback.data[5:]
+
     page = 0
 
-    text = await format_discounts(page)
+    text = await format_discounts(page, sort_by)
 
-    await message.answer(
+    await callback.message.edit_text(
         text,
-        reply_markup=get_keyboard(page)
+        reply_markup=get_keyboard(page, sort_by)
     )
+
+    await callback.answer()
+
 
 
 @router.callback_query(F.data.startswith("discount_"))
 async def paginate(callback: CallbackQuery):
-    page = int(callback.data.split("_")[1])
+    parts = callback.data.split("_")
 
-    text = await format_discounts(page)
+    page = int(parts[1])
+    sort_by = "_".join(parts[2:])
+
+    text = await format_discounts(page, sort_by)
 
     await callback.message.edit_text(
         text,
-        reply_markup=get_keyboard(page)
+        reply_markup=get_keyboard(page, sort_by)
     )
 
     await callback.answer()
